f544d95abb9ce08fd0dce0278170d3a186436833
Adam Vogt <vogt.adam@gmail.com>
2026-03-30 01:13:51 -0400

    start cobyla

--- d.R
+++ d.R
@@ -1,9 +1,8 @@
 # %%
-# library(hetGP)
-# not sure about hetGP here because ccx is deterministic
 # library(targets)
 library(furrr)
 library(glue)
+library(hetGP)
 library(lhs)
 library(nloptr)
 library(processx)
@@ -48,19 +47,32 @@ evalWithMemoization({
   unscaledDesign <- geneticLHS(25, length(varnames))
   scaledDesign <- apply(unscaledDesign, 2, \(row) row * (varmax - varmin) + varmin)
   designStr <- apply(scaledDesign, 1, \(row) paste(varnames, "=", row, sep = "", collapse = " "))
-  future_map_lgl(designStr, runfem, .progress=TRUE)
+  future_map_lgl(designStr, runfem, .progress = TRUE)
 })
 
 # %%
 con <- DBI::dbConnect(RSQLite::SQLite(), "db.sqlite3")
 rq <- tbl(con, "q")
 rp <- tbl(con, "p")
-merge(rp, rq)
+d <- merge(rp, rq)
 # https://coolbutuseless.github.io/2021/12/23/introducing-ggsvg-use-svg-as-ggplot-points/
-# %%
+ggplot(d, aes(ydisp, fy, col = A)) +
+  facet_wrap(cut(C, 3) ~ cut(B, 3)) +
+  geom_path()
 
-#library(ggplot2)
-#ggplot(result2, aes(ydisp, fy, group = A, col = A)) +
-#  geom_line()
-#mm <- lm(fy ~ ydisp + ydisp:A, result2)
-# plot(resid(mm))
+best <- which.max(d$fy)
+x0 <- d[best, words("A B C D E F")]
+
+opt1 <- evalWithMemoization({
+  cobyla(as.numeric(x0), \(x) {
+    runfem(paste(varnames, "=", x, sep = "", collapse = " "))
+    # runfem just returns TRUE/FALSE
+    # d doesn't update, but I can keep the old con and get new elements
+    # if I reevaluate rq,rp and d.
+    # in general I don't want to look for the last row
+    # instead c.py should communicate the p_id somehow
+    # because I'm planning to evaluate candidates in parallel
+    # even if cobyla doesn't do that
+    # hetGP will
+  }, varmin, varmax)
+})
